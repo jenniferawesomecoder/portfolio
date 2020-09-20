d66fdaa329235ccb93c49c00ce14d3283472a23d
jenniferawesomecoder <dogdoglovernumerouno1985@gmail.com>
2020-09-20 13:24:44 +0530

    Updates

---
+++ precache-manifest.23bddd209ee0c0caddc1c223a32a720f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f8145f98f887b334789bc669db4960",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4008075794e4e1d3ff9d",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "04bcffc460f98b81dcd3",
+    "url": "/portfolio/static/css/main.0089cbbe.chunk.css"
+  },
+  {
+    "revision": "4008075794e4e1d3ff9d",
+    "url": "/portfolio/static/js/2.9e79a991.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9e79a991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bcffc460f98b81dcd3",
+    "url": "/portfolio/static/js/main.a436f840.chunk.js"
+  },
+  {
+    "revision": "a7f3ba156162ce79cbc3",
+    "url": "/portfolio/static/js/runtime-main.3154db63.js"
+  },
+  {
+    "revision": "a5a578d36996a8aed9b2567e12add258",
+    "url": "/portfolio/static/media/jennifer.a5a578d3.png"
+  }
+]);
